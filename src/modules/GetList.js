c67ac28fd3a6ebac947aab88eff2b035715f3cc1
jmoseka <jamylam3@gmail.com>
2022-05-11 18:14:36 +0300

    Add sort function to received api data

--- src/modules/GetList.js
+++ src/modules/GetList.js
@@ -1,11 +1,10 @@
 async function getList() {
-  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUdy/scores/';
+  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Zl4d7IVkemOTTVg2fUpp/scores/';
   const response = await fetch(url);
   const reply = await response.json();
   const scoreListContainer = document.querySelector('.score-list-container');
   scoreListContainer.innerHTML = '';
-  const sorted = reply.result.sort((a, b) => a.id - b.id);
-
+  const sorted = reply.result.sort((a, b) => a.score - b.score);
   // eslint-disable-next-line no-restricted-syntax
   for (const scoreList of [...Object(sorted)]) {
     const element = document.createElement('li');
